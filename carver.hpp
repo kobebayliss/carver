a8a146e52346e6a0ed7443aa323e0e751f0e5796
Kobe Bayliss <kobebayliss1@gmail.com>
2026-07-31 18:14:34 +1200

    feat: each heap contains pointer to previous heap in first 8 bytes

--- carver.hpp
+++ carver.hpp
@@ -4,43 +4,45 @@
 
 template <size_t obj_size, size_t heap_size = 16777216, size_t max_free = 65536>  // max_free is maximum number of freed objects (not bytes)
 class Carver {
-	char* heap = nullptr;
+	char* current_heap = nullptr;
 	char* bump_ptr = nullptr;
 	void** free_stack;  // array of freed addresses, not intrusive
 	size_t free_count = 0;
 	
-	char* get_heap() {
-		return static_cast<char*>(mmap(nullptr, heap_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
+	char* get_heap(char* prev_heap = nullptr) {
+		char* new_heap = static_cast<char*>(mmap(nullptr, heap_size + 8, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));  // allocate heap_size + 8 bytes for pointer to previous heap
+		*reinterpret_cast<char**>(new_heap) = prev_heap;
+		return new_heap + 8;
 	}
 public:
 	Carver() {
-		heap = get_heap();
-		bump_ptr = heap;
+		current_heap = get_heap();
+		bump_ptr = current_heap;
 		free_stack = static_cast<void**>(mmap(nullptr, max_free * sizeof(void*), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
 	}
 	~Carver() {
-		if (heap) munmap(heap, heap_size);
+		if (current_heap) munmap(current_heap, heap_size);
 		if (free_stack) munmap(free_stack, max_free * sizeof(void*));
 	}
 	// copy operations (unsupported)
 	Carver(const Carver&) = delete;
 	Carver& operator=(const Carver&) = delete;
 	// move operations
-	Carver(Carver&& other) noexcept : heap(other.heap), bump_ptr(other.bump_ptr), free_stack(other.free_stack), free_count(other.free_count) {
-		other.heap = nullptr;
+	Carver(Carver&& other) noexcept : current_heap(other.current_heap), bump_ptr(other.bump_ptr), free_stack(other.free_stack), free_count(other.free_count) {
+		other.current_heap = nullptr;
 		other.bump_ptr = nullptr;
 		other.free_stack = nullptr;
 		other.free_count = 0;
 	}
 	Carver& operator=(Carver&& other) noexcept {
 		if (this != &other) {
-			if (heap) munmap(heap, heap_size);
+			if (current_heap) munmap(current_heap, heap_size);
 			if (free_stack) munmap(free_stack, max_free * sizeof(void*));
-			heap = other.heap;
+			current_heap = other.current_heap;
 			bump_ptr = other.bump_ptr;
 			free_stack = other.free_stack;
 			free_count = other.free_count;
-			other.heap = nullptr;
+			other.current_heap = nullptr;
 			other.bump_ptr = nullptr;
 			other.free_stack = nullptr;
 			other.free_count = 0;
@@ -52,8 +54,9 @@ public:
 		if (free_count > 0) [[likely]] {  // get address from free stack
 			return free_stack[--free_count];
 		}
-		if (bump_ptr == heap + heap_size) [[unlikely]] {  // reached end of current heap
-			bump_ptr = get_heap();
+		if (bump_ptr == current_heap + heap_size) [[unlikely]] {  // reached end of current heap
+			current_heap = get_heap(current_heap);
+			bump_ptr = current_heap;
 		}
 		void* ptr = bump_ptr;
 		bump_ptr += obj_size;
